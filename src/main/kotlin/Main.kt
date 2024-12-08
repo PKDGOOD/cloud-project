41631a2a2306a9c33272752224783683ca2d79a5
PKDGOOD <sean4787@gmail.com>
2024-12-08 23:44:40 +0900

    가용영역 조회

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
 import aws.sdk.kotlin.runtime.auth.credentials.ProfileCredentialsProvider
 import aws.sdk.kotlin.services.ec2.Ec2Client
+import aws.sdk.kotlin.services.ec2.model.DescribeAvailabilityZonesRequest
 import aws.sdk.kotlin.services.ec2.model.DescribeInstancesRequest
-import java.util.Scanner
+import java.util.*
 
 suspend fun listEc2Instances(region: String) {
     val ec2Client = Ec2Client {
@@ -31,6 +32,31 @@ suspend fun listEc2Instances(region: String) {
     }
 }
 
+suspend fun listAvailableZones(region: String) {
+    val ec2Client = Ec2Client {
+        this.region = region
+        credentialsProvider = ProfileCredentialsProvider(profileName = "default")
+    }
+
+    try {
+        val request = DescribeAvailabilityZonesRequest { }
+        val response = ec2Client.describeAvailabilityZones(request)
+
+        println("Available Zones in region $region:")
+        response.availabilityZones?.forEach { zone ->
+            println(" - Zone Name: ${zone.zoneName}")
+            println("   State: ${zone.state}")
+            println("   Zone ID: ${zone.zoneId}")
+            println("   Messages: ${zone.messages?.joinToString { it.message ?: "" }}")
+            println()
+        }
+    } catch (e: Exception) {
+        println("Error fetching availability zones: ${e.message}")
+    } finally {
+        ec2Client.close()
+    }
+}
+
 suspend fun main() {
     val region = "ap-northeast-2" // 서울 리전
 
@@ -38,12 +64,12 @@ suspend fun main() {
     val idString = Scanner(System.`in`)
 
     var number = 0;
-    var instanceId = ""
     while (true) {
         println("--------------------------------------------")
         println("            AWS  EC2 제어 프로그램            ")
         println("--------------------------------------------")
         println("1. 인스턴스 목록 조회")
+        println("2. 가용 영역 조회")
         println("99. 종료")
         println("--------------------------------------------")
 
@@ -59,6 +85,9 @@ suspend fun main() {
         when (number) {
             1 ->  {
                 listEc2Instances(region)
+            }
+            2 -> {
+                listAvailableZones(region)
             }
             99 -> return
         }
